bf21c7f84a0483bdfbfa30de33005d505cdd2751
prince7866 <71831042+prince7866@users.noreply.github.com>
2021-06-27 01:14:13 +0200

    design changes

--- src/Prices.js
+++ src/Prices.js
@@ -9,6 +9,24 @@ const imageArray = [
   "https://images.unsplash.com/photo-1561525140-c2a4cc68e4bd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80",
 ];
 
+const priceDescription = [
+  "Bildepakke 1 -5 høyoppløste filer 5995 kr",
+
+  "Bildepakke 2 -10 høyoppløste filer 6995 kr",
+
+  "Bildepakke 3 -15 høyoppløste filer 7995 kr",
+
+  " Bildepakke 4 -20 høyoppløste filer 8995 kr",
+
+  "Bildepakke 5 -25 høyuppløste filer 9995 kr",
+
+  "Bildepakke 6 -30 høyuppløste filer 10.995 kr",
+
+  "Bildepakke 7 -35 høyuppløste filer 11.995 kr",
+
+  "Bildepakke 8 -40 høyuppløste filer 12.995 kr,",
+];
+
 function Prices() {
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -18,28 +36,69 @@ function Prices() {
     <div className="prices">
       <MainRow
         title="Priser"
-        subTitle="Et komplett prisliste"
-        description="Nedenfor ser du et veldig bra pris. Det er den beste prisen i hele verden , lorem ipsum Nulla facilisi. Sed sit amet molestie nulla. Curabitur volutpat varius nulla et aliquet. Suspendisse et odio quis tus. Sed sit amet molestie nulla. Curabitur volutpat varius nulla et aliquet. Suspendisse et odio quis tus. Sed sit amet molestie nulla. Curabitur volutpat varius nulla et aliquet. Suspendisse et odio quis tus."
+        subTitle="Her er en oversikt over mine priser og produkter. Et fint utvalg av ulike produkter som passer inn i dagens moderne hjem. Ikke glem å få bildene dine opp på vegg eller i et album slik at de kan nytes til sin fulle rett."
+        // description="Nedenfor ser du et veldig bra pris. Det er den beste prisen i hele verden"
         isColumnFlexDirection
-        contentImage="https://images.unsplash.com/photo-1591681354784-c684e483dae0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=646&q=80"
+        // contentImage="https://images.unsplash.com/photo-1591681354784-c684e483dae0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=646&q=80"
+        isMainPrice
+        hr
       >
         <PriceComponent
           title="Nyfødt"
-          pakke_subTitle_1="Pakke 1 NyFødt"
+          pakke_subTitle_1="Portrett NyFødt"
           pakke_subTitle_2="Pakke 2 NyFødt med Familie"
-          pakke_price_1="3500kr"
+          pakke_price_1="2500kr"
           pakke_price_2="4500kr"
         />
         <PriceComponent
           title="Familie Fotografering"
-          pakke_subTitle_1="Studio"
-          pakke_subTitle_2="Outside"
-          pakke_price_1="3000kr"
-          pakke_price_2="3500kr"
+          pakke_subTitle_1="Liten pakke - 5 bilder"
+          pakke_subTitle_2="Mellom pakke - 10 bilder"
+          pakke_subTitle_3="Stor pakke - 15 bilder"
+          pakke_price_1="2500 ,-"
+          pakke_price_2="3000 ,-"
+          pakke_price_3="4000 ,-"
         />
 
         <PriceComponent
-          title="Bryllup"
+          title="Events"
+          pakke_subTitle_1="Liten pakke - Opptil 3t"
+          pakke_subTitle_2="Stor pakke - Opptil 5t"
+          pakke_price_1="4500,-"
+          pakke_price_2="6000,-"
+        />
+      </MainRow>
+      <MainRow
+        title="Events"
+        subTitle="Priser for fotografering"
+        priceDescription={priceDescription}
+        image="https://images.unsplash.com/photo-1603043647336-e71a50a294c9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=730&q=80"
+        isMarginRightImage
+        isAlignItemsCenter
+        isSubPrice
+        hr
+      ></MainRow>
+      <MainRow
+        title="Nyfødt"
+        subTitle="Nulla facilisi. Sed sit amet molestie nulla. Curabitur volutpat varius nulla"
+        description="tate ipsum a convalctetur. Etiam vel nunc lacus. Cras ultricies porttitor risus ac dapibus. Vivamus eu risus hendrerit, bibendum neque a, euismod eros."
+        image="https://images.unsplash.com/photo-1522771930-78848d9293e8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80"
+        isMarginLeftImage
+        isAlignItemsCenter
+        isMarginRightContent
+        isReverse
+        isSubPrice
+        hr
+      >
+        <PriceComponent
+          title="Brffyllup"
+          pakke_subTitle_1="vielse og ute med alle Digital filler"
+          pakke_subTitle_2="All day med Alle digital filler"
+          pakke_price_1="10000kr"
+          pakke_price_2="18000kr"
+        />
+        <PriceComponent
+          title="loool"
           pakke_subTitle_1="vielse og ute med alle Digital filler"
           pakke_subTitle_2="All day med Alle digital filler"
           pakke_price_1="10000kr"
@@ -47,33 +106,32 @@ function Prices() {
         />
       </MainRow>
       <MainRow
-        title="Bryllup"
-        subTitle="Nulla facilisi. Sed sit amet molestie nulla. Curabitur volutpat varius nulla"
-        description="tate ipsum a convallis. Ut hendrerit magna sed hendrerit molestie. Pellentesque ullamcorper auctor felis. Nullam placerat lacinia ipsum id rutrum. Vivamus aliquet quis metus commodo mollis. Cras commodo tortor a mi placerat mollis. Nam ut ipsum ullamcorper, sollicitudin urna pellentesque, condimentum mauris. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vitae ante eget libero mattis molestie ut quis quam. Phasellus porttitor risus at erat commodo, et viverra turpis commodo. Quisque mollis libero vitae quam mollis consectetur. Etiam vel nunc lacus. Cras ultricies porttitor risus ac dapibus. Vivamus eu risus hendrerit, bibendum neque a, euismod eros."
-        image="https://images.unsplash.com/photo-1603043647336-e71a50a294c9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=730&q=80"
-        isMarginRightImage
-        isAlignItemsCenter
-      />
-      <MainRow
-        title="Nyfødt"
-        subTitle="Nulla facilisi. Sed sit amet molestie nulla. Curabitur volutpat varius nulla"
-        description="tate ipsum a convallis. Ut hendrerit magna sed hendrerit molestie. Pellentesque ullamcorper auctor felis. Nullam placerat lacinia ipsum id rutrum. Vivamus aliquet quis metus commodo mollis. Cras commodo tortor a mi placerat mollis. Nam ut ipsum ullamcorper, sollicitudin urna pellentesque, condimentum mauris. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vitae ante eget libero mattis molestie ut quis quam. Phasellus porttitor risus at erat commodo, et viverra turpis commodo. Quisque mollis libero vitae quam mollis consectetur. Etiam vel nunc lacus. Cras ultricies porttitor risus ac dapibus. Vivamus eu risus hendrerit, bibendum neque a, euismod eros."
-        image="https://images.unsplash.com/photo-1522771930-78848d9293e8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80"
-        isMarginLeftImage
-        isAlignItemsCenter
-        isMarginRightContent
-        isReverse
-      />
-      <MainRow
         title="Familie Fotografering"
-        subTitle="Nulla facilisi. Sed sit amet molestie nulla. Curabitur volutpat varius nulla"
+        subTitle=""
         description="tate ipsum a convallis. Ut hendrerit magna sed hendrerit molestie. Pellentesque ullamcorper auctor felis. Nullam placerat lacinia ipsum id rutrum. Vivamus aliquet quis metus commodo moll ultricies porttitor risus ac dapibus. Vivamus eu risus hendrerit, bibendum neque a, euismod eros."
-        image="https://images.unsplash.com/photo-1590171980804-fa50d5ca4524?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80"
+        // image="https://images.unsplash.com/photo-1590171980804-fa50d5ca4524?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80"
         isMarginRightImage
         isAlignItemsCenter
         isMarginRightContent
         imageArray={imageArray}
-      />
+        isSubPrice
+        hr
+      >
+        <PriceComponent
+          title="Brffyllup"
+          pakke_subTitle_1="vielse og ute med alle Digital filler"
+          pakke_subTitle_2="All day med Alle digital filler"
+          pakke_price_1="10000kr"
+          pakke_price_2="18000kr"
+        />
+        <PriceComponent
+          title="loool"
+          pakke_subTitle_1="vielse og ute med alle Digital filler"
+          pakke_subTitle_2="All day med Alle digital filler"
+          pakke_price_1="10000kr"
+          pakke_price_2="18000kr"
+        />
+      </MainRow>
     </div>
   );
 }
